ec167fe534db3e689c29a1d71265a18db412d16a
+ <stingeyandrewstown@live.com>
2023-05-11 19:52:44 -0500

    yes

--- app/page.js
+++ app/page.js
@@ -1,95 +1,28 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+
+import {useEffect, useState} from "react"
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [pools, setPools] = useState([])
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  let box
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  useEffect(() => {
+    box = document.querySelector("#box")
+    document.addEventListener("click", (e) => {
+      setPools(pools => [...pools, {x: e.x, y: e.y, color: window.getComputedStyle(box).backgroundColor}])
+      setTimeout(() => {
+        setPools(pools => (pools.slice(1, pools.length)))
+      }, 800)
+    })
+  }, [])
+  
+  return (<>
+    <div id="box"/>
+    <img id="lebron" src="./lebron.webp"/>
+    <img id="rockstarfreddy" src="./rockstarfreddy.webp"/>
+    {pools.map((pool, i) => (
+      <div className="pool" key={`pool-${i}`} style={{top: `${pool.y}px`, left: `${pool.x}px`, borderColor: pool.color}}/>
+    ))}
+  </>)
 }
